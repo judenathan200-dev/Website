d1be511a1b52d0e57bce3e0128996cd9518e1b28
judenathan200-dev <judenathan200@gmail.com>
2026-07-21 23:22:08 +0100

    web 3

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const navMenu = document.getElementById('nav-menu');
 
     if (menuToggle && navMenu) {
+        // Open/Close when clicking the 3 lines
         menuToggle.addEventListener('click', function() {
             navMenu.classList.toggle('active');
+        });
+
+        // Close the menu when the user starts scrolling
+        window.addEventListener('scroll', function() {
+            if (navMenu.classList.contains('active')) {
+                navMenu.classList.remove('active');
+            }
         });
     }
 });
